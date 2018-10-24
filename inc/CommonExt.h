9259402571a46c6a965776e20b852ae6509e2aae
lhyuan <1028417@qq.com>
2018-10-25 03:56:52 +0800

    2.1.4

--- inc/CommonExt.h
+++ inc/CommonExt.h
@@ -1,6 +1,29 @@
 
 #pragma once
 
+#include <string>
+#include <sstream>
+
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+
+#include <algorithm>
+
+#include <functional>
+
+#include <thread>
+#include <future>
+
+#include <stdlib.h>
+
+#include <fstream>
+
+#include <afxcmn.h>			// MFC 对 Windows 公共控件的支持
+
+using namespace std;
+
 #ifdef __CommonPrj
 #define __CommonPrjExt __declspec(dllexport)
 #else
@@ -13,7 +36,80 @@
 #define __DllFile(_x) (_x + string(".dll"))
 #endif
 
-#include <CommonDef.h>
+enum class E_TrackMouseEvent
+{
+	LME_MouseMove
+	, LME_MouseHover
+	, LME_MouseLeave
+};
+
+using CB_TrackMouseEvent = function<void(E_TrackMouseEvent eMouseEvent, const CPoint& point)>;
+
+#define	__BackSlant L'\\'
+
+#define __Ensure(x) \
+	if (!(x)) \
+	{ \
+		return; \
+	}
+
+#define __Assert(x) \
+	if (!(x)) \
+	{ \
+		ASSERT(FALSE); \
+		return; \
+	}
+
+#define __EnsureReturn(x, y) \
+	if (!(x)) \
+	{ \
+		return y; \
+	}
+
+#define __AssertReturn(x, y) \
+	if (!(x)) \
+	{ \
+		ASSERT(FALSE); \
+		return y; \
+	}
+
+
+#define __EnsureContinue(x) \
+	if (!(x)) \
+	{ \
+		continue; \
+	}
+
+#define __AssertContinue(x) \
+	if (!(x)) \
+	{ \
+		ASSERT(FALSE); \
+		continue; \
+	}
+
+#define __EnsureBreak(x) \
+	if (!(x)) \
+	{ \
+		break; \
+	}
+
+#define __AssertBreak(x) \
+	if (!(x)) \
+	{ \
+		ASSERT(FALSE); \
+		break; \
+	}
+
+#define DECLARE_SINGLETON(_CLASS) \
+	public: \
+		static _CLASS& inst() \
+		{ \
+			static _CLASS inst; \
+			return inst; \
+		} \
+\
+	private: \
+		_CLASS();
 
 #include <util.h>
 
