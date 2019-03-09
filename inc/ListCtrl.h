44f4e05b6f94feff10c96a3132f10d28747de27d
lhyuan <1028417@qq.com>
2019-03-09 23:25:04 +0800

    2.1.38

--- inc/ListCtrl.h
+++ inc/ListCtrl.h
@@ -240,7 +240,10 @@ private:
 	template <bool _clear_other>
 	void _SetItemTexts(UINT uItem, const vector<wstring>& vecText, const wstring& strPrefix = L"");
 
-	virtual void getRenameText(UINT uItem, CString& cstrRenameText) {}
+	virtual bool GetRenameText(UINT uItem, wstring& strRenameText)
+	{
+		return true;
+	}
 
 	virtual void OnListItemRename(UINT uItem, const CString& cstrNewText) {};
 
