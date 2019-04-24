e832add5dee0ba449d9ff625d952f438a00b5ee6
lhyuan <1028417@qq.com>
2019-04-25 04:49:58 +0800

    2.1.44

--- inc/util/mtutil.h
+++ inc/util/mtutil.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#ifdef _MSC_VER
 #pragma warning(disable: 4275)
+#endif
 
 #include <mutex>
 using namespace std;
--- inc/util/util.h
+++ inc/util/util.h
@@ -1,7 +1,9 @@
 
 #pragma once
 
+#ifdef _MSC_VER
 #pragma warning(disable: 4251)
+#endif
 
 #include <stdint.h>
 #include <stddef.h>
