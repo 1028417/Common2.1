d6959879bad9e05233d9d959126b194781606fe1
lhyuan <1028417@qq.com>
2019-11-14 04:58:57 +0800

    2.1.65

--- src/util/cpp/jsonutil.cpp
+++ src/util/cpp/jsonutil.cpp
@@ -57,7 +57,7 @@ bool jsonutil::get(const JValue& jValue, int& nRet)
     return true;
 }
 
-bool jsonutil::get(const JValue& jValue, UINT& uRet)
+bool jsonutil::get(const JValue& jValue, unsigned int& uRet)
 {
     if (jValue.isNull())
     {
@@ -68,3 +68,29 @@ bool jsonutil::get(const JValue& jValue, UINT& uRet)
 
     return true;
 }
+
+#ifdef JSON_HAS_INT64
+bool jsonutil::get(const JValue& jValue, Json::Value::Int64& nRet)
+{
+    if (jValue.isNull())
+    {
+        return false;
+    }
+
+    nRet = jValue.asInt64();
+
+    return true;
+}
+
+bool jsonutil::get(const JValue& jValue, Json::Value::UInt64& uRet)
+{
+    if (jValue.isNull())
+    {
+        return false;
+    }
+
+    uRet = jValue.asUInt64();
+
+    return true;
+}
+#endif
