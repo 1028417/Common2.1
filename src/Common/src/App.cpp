2ddaa6ba8a111ce75de9ea957623211a76706cfc
lhyuan <1028417@qq.com>
2019-11-13 15:48:43 +0800

    2.1.65

--- src/Common/src/App.cpp
+++ src/Common/src/App.cpp
@@ -134,9 +134,6 @@ E_DoEventsResult CMainApp::DoEvents(bool bOnce)
 
 BOOL CMainApp::InitInstance()
 {
-	extern void InitMinDump();
-	InitMinDump();
-
 	// 如果一个运行在 Windows XP 上的应用程序清单指定要
 	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
 	//则需要 InitCommonControlsEx()。  否则，将无法创建窗口。
