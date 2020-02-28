b23e6ef9d6fc12a2becc47585a1082d43ac4bb60
musicro <1028418@qq.com>
2020-02-29 02:34:39 +0800

    2.1.73

--- src/util/cpp/tmutil.cpp
+++ src/util/cpp/tmutil.cpp
@@ -1,39 +1,21 @@
 
 #include "util.h"
 
-time32_t tmutil::time32()
-{
-#if __windows
-	return _time32(NULL);
-#else
-	return (time32_t)time(NULL);
-#endif
-}
-
-time64_t tmutil::time64()
-{
-#if __windows
-	return _time64(NULL);
-#else
-	return time(NULL);
-#endif
-}
-
 void tmutil::getCurrentTime(int& nHour, int& nMinute)
 {
     tagTM tm;
-    timeToTM(time32(), tm);
+    timeToTM(__time, tm);
 
     nHour = tm.tm_hour;
     nMinute = tm.tm_min;
 }
 
-bool tmutil::timeToTM(time32_t tTime, tagTM& tm)
+bool tmutil::timeToTM(time_t tTime, tagTM& tm)
 {
 	struct tm _tm;
 
 #if __windows
-    if (_localtime32_s(&_tm, &tTime))
+    if (localtime_s(&_tm, &tTime))
 	{
 		return false;
 	}
@@ -103,27 +85,9 @@ string tmutil::formatTime(const string& strFormat, time32_t tTime)
 }
 
 #if __windows
-bool tmutil::timeToTM(time64_t time, tagTM& tm)
-{
-    struct tm _tm;
-    if (_localtime64_s(&_tm, &time))
-    {
-        return false;
-    }
-
-    tm = _tm;
-
-    return true;
-}
-
 template <class T>
 inline static T _formatTime64T(const T& strFormat, time64_t tTime)
 {
-    if (-1 == tTime)
-    {
-        tTime = tmutil::time64();
-    }
-
     tm atm;
     if (_localtime64_s(&atm, &tTime))
     {
