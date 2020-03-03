3c37f25ec590524b86fe414d429c47a788c122f4
musicro <1028418@qq.com>
2020-03-04 07:28:31 +0800

    2.1.74

--- src/Common/src/App.cpp
+++ src/Common/src/App.cpp
@@ -452,7 +452,7 @@ LRESULT CMainApp::SendModuleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	CMainApp *pMainApp = GetMainApp();
 	__EnsureReturn(pMainApp, 0);
 
-	CWaitCursor WaitCursor;
+	__waitCursor;
 
 	LRESULT lResult = pMainApp->getController().handleModuleMessage(uMsg, wParam, lParam);
 	if (0 != lResult)
@@ -477,7 +477,7 @@ void CMainApp::BroadcastModuleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	CMainApp *pMainApp = GetMainApp();
 	__Ensure(pMainApp);
 
-	CWaitCursor WaitCursor;
+	__waitCursor;
 
 	(void)pMainApp->getController().handleModuleMessage(uMsg, wParam, lParam);
 
